c12aaa59ab1ec4d307409c0f04e6437655e53c45
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2024-05-09 20:01:09 +0530

    stage changes

--- frontend/Travel_Booking/src/App.jsx
+++ frontend/Travel_Booking/src/App.jsx
@@ -1,32 +1,17 @@
-<<<<<<< HEAD
-import SignInSide from "./components/SignIn/SignIn";
 // import JoySignInSideTemplate from "./components/SignIn/SignIn";
-=======
-
-import JoySignInSideTemplate from "./components/SignIn/SignIn";
->>>>>>> 70df03338ef5628f8ce981084834f89e69906bb9
 // import SignUp from "./components/SignUp/SignUp";
-import { useState } from 'react'
-import './App.css'
-import SignUp from "./components/SignUp/SignUp";
-import Homepage from './components/homepage/Homepage';
+// import { useState } from 'react'
+import "./App.css";
+// import SignUp from "./components/SignUp/SignUp";
+// import SignUp from "./components/SignUp/SignUp";
+import Homepage from "./components/homepage/Homepage";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <SignInSide />{" "}
-=======
-    <div className='app'>
-   
-  
->>>>>>> 70df03338ef5628f8ce981084834f89e69906bb9
+    <div className="app">
+      <Homepage />
     </div>
-  )
-}
-    
-   
-
-
+  );
+};
 
 export default App;
