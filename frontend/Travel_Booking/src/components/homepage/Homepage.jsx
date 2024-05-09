03b8a6400c584e4e2a95b6be9f6ae99f67b064b6
Logeshkumar <logesh07cbe@gmail.com>
2024-05-09 19:27:46 +0530

    updted car image

--- frontend/Travel_Booking/src/components/homepage/Homepage.jsx
+++ frontend/Travel_Booking/src/components/homepage/Homepage.jsx
@@ -1,39 +1,37 @@
 import React from "react";
-import {AppBar, Menu, MenuItem, MenuList, Toolbar, Typography} from '@mui/material'
 
-import "./Homepage.css"
-import redcar from './redCar.png'
+import "./Homepage.css";
+import { AppBar, Button, MenuItem, MenuList, Toolbar } from "@mui/material";
+
+import redcar from "./redCar.png";
 const Homepage = () => {
   return (
     <div className="homepage">
-      <AppBar position="static" sx={{backgroundColor:"black"}}>
+      <div className="appbar-caranime">
+      <AppBar position="static" sx={{ backgroundColor: "rgba(0, 0, 0, 0.85)" }}>
         <Toolbar>
-          <MenuItem>
-          CARLASELVOM
-          </MenuItem>
-          <MenuList sx={{display:'flex',justifyContent:'right',width:'100vw'}}>
+          <MenuItem>CARLASELVOM</MenuItem>
+          <MenuList
+            sx={{ display: "flex", justifyContent: "right", width: "100vw" }}
+          >
             <MenuItem>Profile</MenuItem>
             <MenuItem>Logout</MenuItem>
           </MenuList>
-
         </Toolbar>
       </AppBar>
       <div className="Homepage-carLogo">
-          <img className="imgcar" src={redcar}/>
+        <img className="imgcar" src={redcar} />
+      </div>
+
       </div>
       <div className="Homepage-outer1">
         <div className="Homepage-outer1-bookride">
-          <Typography>Book Ride</Typography> 
+          <Button>Book Ride</Button>
         </div>
         <div className="Homepage-outer1-publishride">
-          <Typography>Publish Ride</Typography> 
-          
-        
-
+          <Button>Publish Ride</Button>
         </div>
-      </div >
-
-      
+      </div>
     </div>
   );
 };
