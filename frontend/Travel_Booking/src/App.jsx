801e99c7cda49d7f3f5a9574c01853d82463de8c
Logeshkumar <logesh07cbe@gmail.com>
2024-05-09 20:06:34 +0530

    commited structure alteration

--- frontend/Travel_Booking/src/App.jsx
+++ frontend/Travel_Booking/src/App.jsx
@@ -5,11 +5,13 @@ import { useState } from 'react'
 import './App.css'
 import SignUp from "./components/SignUp/SignUp";
 import Homepage from './components/homepage/Homepage';
+import SignInSide from "./components/SignIn/SignIn";
 
 const App = () => {
   return (
     <div className='app'>
-   
+      <Homepage></Homepage>
+
   
     </div>
   )
