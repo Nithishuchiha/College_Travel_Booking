7690c5b51866b1bb394bc0d3742729b5e4b63c17
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2024-05-12 20:57:59 +0530

    fix: to post the user

--- backend/src/main/java/com/college_project/backend/controller/SignupController.java
+++ backend/src/main/java/com/college_project/backend/controller/SignupController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.college_project.backend.model.UserDetails;
 import com.college_project.backend.service.SignupService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class SignupController {
     
@@ -20,7 +23,7 @@ public class SignupController {
         this.signupService = signupService;
     }
 
-    @GetMapping("/signup")
+    @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody UserDetails userDetails) {
         try{
             signupService.saveUser(userDetails);
