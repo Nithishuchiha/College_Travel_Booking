4c3060117f339cde270259d209524b5c3a0c8ad9
Pavithran G <119714223+G-Pavithran-dev@users.noreply.github.com>
2024-05-12 22:01:30 +0530

    Update SignupController.java

--- backend/src/main/java/com/college_project/backend/controller/SignupController.java
+++ backend/src/main/java/com/college_project/backend/controller/SignupController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.college_project.backend.model.UserDetails;
 import com.college_project.backend.service.SignupService;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class SignupController {
     
